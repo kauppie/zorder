8f7fcf9ee32c5c971390894b001a7e273296567b
Elias Kauppi <elkku.kauppi@gmail.com>
2023-09-23 01:50:33 +0300

    Remove old functions

--- benches/zorder_bench.rs
+++ benches/zorder_bench.rs
@@ -1,17 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
-use zorder::{
-    array_coord_of, array_index_of, bmi2, coord_of, coord_of_64, coord_of_dual_pass, index_of,
-    index_of_64, index_of_64_dual_pass,
-};
+use zorder::{array_coord_of, array_index_of, bmi2};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("coord_of_dual_pass", |b| {
-        b.iter(|| coord_of_dual_pass(black_box(23748761)))
-    });
-
-    c.bench_function("index_of", |b| b.iter(|| index_of(black_box((2374, 8761)))));
-
     c.bench_function("array_index_of_u64", |b| {
         b.iter(|| array_index_of(black_box([2765132312347u64, 769718192876348788u64])))
     });
@@ -60,8 +51,6 @@ fn criterion_benchmark(c: &mut Criterion) {
         b.iter(|| array_coord_of::<_, 2>(black_box(23776u16)))
     });
 
-    c.bench_function("coord_of", |b| b.iter(|| coord_of(black_box(23748761))));
-
     #[cfg(target_arch = "x86_64")]
     {
         if is_x86_feature_detected!("bmi2") {
@@ -74,18 +63,6 @@ fn criterion_benchmark(c: &mut Criterion) {
             });
         }
     }
-
-    c.bench_function("index_of_64", |b| {
-        b.iter(|| index_of_64(black_box((23744732, 87611678))))
-    });
-
-    c.bench_function("index_of_64_dual_pass", |b| {
-        b.iter(|| index_of_64_dual_pass(black_box((23744732, 87611678))))
-    });
-
-    c.bench_function("coord_of_64", |b| {
-        b.iter(|| coord_of_64(black_box(2374473287611678)))
-    });
 
     #[cfg(target_arch = "x86_64")]
     {
